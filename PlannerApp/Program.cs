5763ef875435700c8286f424567becc4c2e936d7
S114289 <S114289@LTHD204S2912XQ2>
2024-10-08 14:29:36 -0500

    project work from 10/8

--- PlannerApp/Program.cs
+++ PlannerApp/Program.cs
@@ -11,15 +11,24 @@ namespace PlanningApp
     {
         static void Main(string[] args)
         {
+            
             Boolean keepPlanning = true;
             do
             {
-                Console.WriteLine("Welcome to the Student Planner! What would you like to track assignments, internship applications, or events? Enter A for assignments, I for internships, E for events, S to stop using the app!");
+                Console.Write("Welcome to the Student Planner! What would you like to track assignments, internship applications, or events? Enter A \nfor assignments, I for internships, E for events, S to stop using the app! ");
                 String plannerType = Console.ReadLine().ToUpper();
-                if (plannerType.Equals("L"))
+                if (plannerType.Equals("S"))
                 {
                     keepPlanning = false;
                     break;
+                }
+                else if (plannerType.Equals("A"))
+                {
+                    AssignmentTracking();
+                }
+                else if (plannerType.Equals("I"))
+                {
+                    InternshipTracking();
                 }
 
             }
@@ -29,20 +38,49 @@ namespace PlanningApp
 
         static void AssignmentTracking()
         {
-            Console.WriteLine("How many assignments do you have to enter?");
-            String[] assignmentNames = new String[Convert.ToInt32(Console.ReadLine())];
-            int[] assignmentDates = new int[Convert.ToInt32(Console.ReadLine())];
+            Console.Write("How many assignments do you have to enter?");
+            int totalAssignments = Convert.ToInt32(Console.ReadLine());
+            String[] assignmentNames = new String[totalAssignments];
+            int[] assignmentDates = new int[totalAssignments];
             int i = 0;
             while (i < assignmentNames.Length)
             {
-                Console.WriteLine("Enter the name of the assignment ");
+                Console.Write("Enter the name of the assignment: ");
                 assignmentNames[i] = Console.ReadLine();
+                Console.Write("Enter the how many days until the assignment is due: ");
+                assignmentDates[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
             }
+            for (int j = 0; j < assignmentNames.Length; j++)
+            {
+                Console.WriteLine(assignmentNames[j] + " - due in " + assignmentDates[j] + " days");
+            }
+            Console.WriteLine();
         }
 
         static void InternshipTracking()
         {
-
+            Console.Write("How many applicatinos do you have to enter?");
+            int totalApplications = Convert.ToInt32(Console.ReadLine());
+            String[] applicationNames = new String[totalApplications];
+            String [] internshipTypes = new String[totalApplications];
+            int[] applicationsDates = new int[totalApplications];
+            int i = 0;
+            while (i < applicationNames.Length)
+            {
+                Console.Write("Enter the name of the internship: ");
+                applicationNames[i] = Console.ReadLine();
+                Console.Write("Enter what field or department the internship is under: ");
+                internshipTypes[i] = Console.ReadLine();
+                Console.Write("Enter the how many days until the assignment is due: ");
+                applicationsDates[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            for (int j = 0; j < applicationNames.Length; j++)
+            {
+                Console.WriteLine(applicationNames[j] + " internship of " + internshipTypes[j] + " due in " + applicationsDates[j] + " days");
+            }
+            Console.WriteLine();
         }
 
         static void EventTracking()
